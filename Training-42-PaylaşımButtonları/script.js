a7cd5c4670028bdf3d9512ab5f842ff321164c8f
Ferhat <ferhat.cigsarr@gmail.com>
2025-05-09 09:39:58 +0300

    New Project

---
+++ Training-42-PaylaşımButtonları/script.js
@@ -0,0 +1,25 @@
+const ShareBtn = document.querySelector(".share-button");
+
+// Sosyal Medya Paylaşımı kutusunu açıp kapatmak için bir işlev
+
+const toogleScoials = () => {
+  const socialWrapper = document.querySelector(".socials-wrapper");
+
+  // Paylaşa butonu içindeki  resmi seç
+
+  const ShareButtonImage = ShareBtn.querySelector("img");
+
+  //Sosyal medya paylaşımı kutusunun görünürlüğü değiştir (aç/kapa)
+
+  socialWrapper.classList.toggle("active");
+
+  // Paylaşma butonu resminin kaynak yolunu kontrol et
+
+  if (ShareButtonImage.src.includes("share")) {
+    ShareBtn.src = "img/close.png";
+  } else {
+    ShareBtn.src = "img/share.png";
+  }
+};
+
+ShareBtn.addEventListener("click", toogleScoials);
